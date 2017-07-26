cbbf3ed9abe9211a9bae2f54f8b076a8d374a70a
antzu <andres.aava@gmail.com>
2017-07-26 18:51:58 +0300

    starting on signup

---
+++ src/components/auth/signup.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { reduxForm } from 'redux-form';
+import * as actions from '../../actions';
+
+class Signup extends Component {
+	render() {
+		return (
+
+
+				<div>
+
+						Signup
+				</div>
+
+			);
+	}
+}
+
+export default Signup;
